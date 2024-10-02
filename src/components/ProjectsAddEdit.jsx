6298f7aa2caf795d1308905bb65feb6da63b3c0b
TimmyRich <gotim02@gmail.com>
2024-10-02 15:39:02 +1000

    Updated code styling of the ProjectAddEdit component and used required tag instead of manually validating input fields.

--- src/components/ProjectsAddEdit.jsx
+++ src/components/ProjectsAddEdit.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 
-const ProjectsAddEdit = ({ onSaveProject , project: targetProject}) => {
+const ProjectsAddEdit = ({ onSaveProject , project: targetProject }) => {
 
   // Re-render form if targetProject changes
   useEffect(() => {
@@ -16,7 +16,7 @@ const ProjectsAddEdit = ({ onSaveProject , project: targetProject}) => {
     }
   }, [targetProject]);
 
-// Check if targetProject is null, initalise empty project if true
+  // Initialize state hooks
   const [title, setTitle] = useState(targetProject ? targetProject.title : '');
   const [description, setDescription] = useState(targetProject ? targetProject.description : '');
   const [instructions, setInstructions] = useState(targetProject ? targetProject.instructions : '');
@@ -24,12 +24,7 @@ const ProjectsAddEdit = ({ onSaveProject , project: targetProject}) => {
   const [display, setDisplay] = useState(targetProject ? targetProject.homescreen_display : '0');
   const [scoring, setScoring] = useState(targetProject ? targetProject.participant_scoring : '0');
   const [published, setPublished] = useState(targetProject ? targetProject.is_published : false);
-  
-  /**
-   * clearInputs
-   * 
-   * Sets the state hooks for the target project back to their default values.
-   */
+
   const clearInputs = () => {
     setTitle('');
     setDescription('');
@@ -40,98 +35,98 @@ const ProjectsAddEdit = ({ onSaveProject , project: targetProject}) => {
     setPublished(false);
   }
 
-  /**
-   * handleSubmit
-   * 
-   * Function to handle functionality of submit button. 
-   * 
-   * @param {*} e Event sent from the submit button.
-   * @returns None.
-   */
   const handleSubmit = async (e) => {
     e.preventDefault(); // Prevent page reload
 
-    // Trim inputs and check for empty fields
-    if (!title.trim()) {
-      alert("Title cannot be empty");
-      return;
-    }
-    if (!description.trim()) {
-      alert("Description cannot be empty");
-      return;
-    }
-    if (!instructions.trim()) {
-      alert("Instructions cannot be empty");
-      return;
-    }
-
     if (onSaveProject.name === 'addProject') {
-
-      //Create template of new project
       const displayVal = display;
       const scoringVal = scoring;
       const newProject = {
-        title: title,
-        description: description,
-        instructions: instructions,
+        title,
+        description,
+        instructions,
         initial_clue: initialClue,
         homescreen_display: displayVal,
         is_published: published,
-        participant_scoring: scoringVal
+        participant_scoring: scoringVal,
       };
-      
-      // Make API call to add project to database
+
+      // Call API to add project
       onSaveProject(newProject);
     } else {
-      //Create edited project
-      const displayVal = display
-      const scoringVal = scoring
+      const displayVal = display;
+      const scoringVal = scoring;
       const editedProject = {
-        title: title,
-        description: description,
-        instructions: instructions,
+        title,
+        description,
+        instructions,
         initial_clue: initialClue,
         homescreen_display: displayVal,
         is_published: published,
         participant_scoring: scoringVal,
         id: targetProject.id,
-        username: targetProject.username
+        username: targetProject.username,
       };
 
-      // Make API call to edit project in our database
-      onSaveProject(editedProject)
-  }
+      // Call API to edit project
+      onSaveProject(editedProject);
+    }
 
-    clearInputs()
+    clearInputs();
   };
 
   return (
-    <div className="container p-5 text-dark"> {/* Added text-dark class */}
+    <div className="container p-5 text-dark">
       <h2 className="text-center mb-4">My Project</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label className="form-label">Title</label>
-          <input type="text" className="form-control" onChange={(e) => setTitle(e.target.value)} value={title} />
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
           <div className="form-text">The name of your project.</div>
         </div>
         <div className="mb-3">
           <label className="form-label">Description</label>
-          <input type="text" className="form-control" onChange={(e) => setDescription(e.target.value)} value={description} />
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+            required
+          />
           <div className="form-text">Describe your project. This is not shown to participants.</div>
         </div>
         <div className="mb-3">
           <label className="form-label">Instructions</label>
-          <textarea className="form-control" onChange={(e) => setInstructions(e.target.value)} value={instructions} />
+          <textarea
+            className="form-control"
+            onChange={(e) => setInstructions(e.target.value)}
+            value={instructions}
+            required
+          />
           <div className="form-text">Give your participants some instructions for this project.</div>
         </div>
         <div className="mb-3">
           <label className="form-label">Initial Clue</label>
-          <textarea className="form-control" onChange={(e) => setInitialClue(e.target.value)} value={initialClue} />
+          <textarea
+            className="form-control"
+            onChange={(e) => setInitialClue(e.target.value)}
+            value={initialClue}
+          />
           <div className="form-text">Optional. Provide a small clue to help get participants started.</div>
         </div>
         <div className="mb-3">
           <label className="form-label">Homescreen Display</label>
-          <select className="form-select" onChange={(e) => setDisplay(e.target.value)} value={display}>
+          <select
+            className="form-select"
+            onChange={(e) => setDisplay(e.target.value)}
+            value={display}
+          >
             <option value="Display initial clue">Display Initial Clue</option>
             <option value="Display all locations">Display All Locations</option>
           </select>
@@ -139,7 +134,11 @@ const ProjectsAddEdit = ({ onSaveProject , project: targetProject}) => {
         </div>
         <div className="mb-3">
           <label className="form-label">Participant Scoring</label>
-          <select className="form-select" onChange={(e) => setScoring(e.target.value)} value={scoring}>
+          <select
+            className="form-select"
+            onChange={(e) => setScoring(e.target.value)}
+            value={scoring}
+          >
             <option value="Not Scored">Not Scored</option>
             <option value="Number of Scanned QR Codes">Number of Scanned QR Codes</option>
             <option value="Number of Locations Entered">Number of Locations Entered</option>
@@ -147,11 +146,16 @@ const ProjectsAddEdit = ({ onSaveProject , project: targetProject}) => {
           <div className="form-text">Select how participants will be scored in this project.</div>
         </div>
         <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" onChange={(e) => setPublished(e.target.checked)} checked={published} />
+          <input
+            type="checkbox"
+            className="form-check-input"
+            onChange={(e) => setPublished(e.target.checked)}
+            checked={published}
+          />
           <label className="form-check-label">Published</label>
         </div>
         <div className="text-center">
-          <button type="submit" className="btn btn-primary" data-bs-dismiss="modal">Save Project</button>
+          <button type="submit" className="btn btn-primary">Save Project</button>
         </div>
       </form>
     </div>
@@ -160,7 +164,8 @@ const ProjectsAddEdit = ({ onSaveProject , project: targetProject}) => {
 
 ProjectsAddEdit.propTypes = {
   onSaveProject: PropTypes.func.isRequired,
-  project: PropTypes.any
+  project: PropTypes.any,
 };
 
 export default ProjectsAddEdit;
+
