04e259b19cdda5dc7c4e2ce48f18fc8dd1b68e7f
TimmyRich <gotim02@gmail.com>
2024-10-03 14:49:19 +1000

    Updated the look of Preview to be more consistent with the rest of the website. Modified the look of the smartphone to be more visible against the dark background.

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,14 +1,19 @@
 import { useParams } from "react-router-dom";
-
+import Smartphone from "./SmartPhone";
 
 const Preview = () => {
     const { projectId } = useParams(); // This will retrieve the projectId from the URL
     
     return (
-      <div className="container-fluid text-light px-5" style={{ backgroundColor: '#1d1d1d' }}>
-        <h2 className="display-4 fw-bold mb-4" style={{ fontFamily: 'Roboto, sans-serif' }}>{`Preview for project ${projectId}`}</h2>
-      </div>
+        <div className="container-fluid p-5 text-light" style={{ backgroundColor: '#1d1d1d' }}>
+            <h2 className="display-4 fw-bold mb-4" style={{ fontFamily: 'Roboto, sans-serif' }}>
+                {`Preview for Project ${projectId}`}
+            </h2>
+            <div className="d-flex justify-content-center">
+                <Smartphone />
+            </div>
+        </div>
     );
-  };
-  
-  export default Preview;
+};
+
+export default Preview;
---
+++ src/components/Smartphone.jsx
@@ -0,0 +1,62 @@
+const Smartphone = () => {
+    return (
+      <div
+        style={{
+          width: "300px",
+          height: "600px",
+          backgroundColor: "#444", // Changed to a lighter color
+          borderRadius: "30px",
+          position: "relative",
+          boxShadow: "0 0 20px rgba(0, 0, 0, 0.5)",
+        }}
+        className="d-flex flex-column justify-content-between p-3"
+      >
+        {/* Smartphone Screen */}
+        <div
+          style={{
+            width: "100%",
+            height: "90%",
+            backgroundColor: "#1d1d1d",
+            borderRadius: "30px 30px 0 0",
+            color: "white",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            boxShadow: "inset 0 0 10px rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          <h3>Smartphone Screen</h3>
+          <p>Your app content goes here!</p>
+        </div>
+  
+        {/* Home Button */}
+        <div
+          style={{
+            width: "60px",
+            height: "10px",
+            backgroundColor: "#fff",
+            borderRadius: "5px",
+            margin: "20px auto 0", // Centering the button
+          }}
+        />
+  
+        {/* Front Camera */}
+        <div
+          style={{
+            width: "15px",
+            height: "15px",
+            backgroundColor: "#fff",
+            borderRadius: "50%",
+            position: "absolute",
+            top: "20px",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        />
+      </div>
+    );
+  };
+  
+  export default Smartphone;
+  
